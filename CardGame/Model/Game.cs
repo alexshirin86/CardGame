8bdc8da4224957869525dde076b21e6316e36a8e
Lucky <Lucky@LUCKY-PC>
2021-05-21 17:46:18 +0300

    Update

---
+++ CardGame/Model/Game.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace CardGame.Model
+{
+    public class Game
+    {
+        public void Rule()
+        {
+            throw new System.Exception("Not implemented");
+        }
+
+        public void RuleGame()
+        {
+            throw new System.Exception("Not implemented");
+        }
+
+        public void AddDischarge()
+        {
+            throw new System.Exception("Not implemented");
+        }
+
+    }
+}
